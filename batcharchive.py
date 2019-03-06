e6baa379099be7c22d55f59d84c3319b6bd38b87
mauricecyril <25261554+mauricecyril@users.noreply.github.com>
2019-03-06 12:50:09 -0500

    Update batcharchive.py

--- batcharchive.py
+++ batcharchive.py
@@ -1,14 +1,14 @@
 import savepagenow
 import time
 
-# Add entries to dictionary. Key = Website URL
-websitelist = {"www.example.com",
+# Add entries to a list of websites
+websitelist = ["www.example.com",
                "www.example2.com",
-                }
+              ]
 
-for k in websitelist.items():
+for k in websitelist:
     archive_url = savepagenow.capture_or_cache(k)
     print(archive_url)
 
-    # Run the script every 30 seconds
-    time.sleep(30)
+    # Run each capture every 5 seconds
+    time.sleep(5)
