0d7160cccd94b9f739068932276fad4f462f6a37
mauricecyril <25261554+mauricecyril@users.noreply.github.com>
2019-03-06 14:48:43 -0500

    Update batcharchive.py

--- batcharchive.py
+++ batcharchive.py
@@ -6,6 +6,16 @@ websitelist = ["www.example.com",
                "www.example2.com",
               ]
 
+# Or create a textfile called websitefile.txt
+# and put a seperate url on each line. Then
+# save the text file to the same path as this
+# python script.
+#
+# Open a text file with the list in Read Mode
+websitefile = open("websitefile.txt", "r").read().splitlines()
+
+# if using the text file method, replace "websitelist" in
+# the following loop to "websitefile"
 for k in websitelist:
     archive_url = savepagenow.capture_or_cache(k)
     print(archive_url)
